42aa2b2510bf0143ea38291f59982c7f90e38818
Jclavo <jclavotafur@gmail.com>
2020-02-27 19:30:27 -0300

    response model

--- src/modules/utility/models/index.ts
+++ src/modules/utility/models/index.ts
@@ -1,1 +1,2 @@
 export * from './utility.model';
+export * from './response.model';
---
+++ src/modules/utility/models/response.model.ts
@@ -0,0 +1,8 @@
+export class Response {
+
+    public status: boolean = false;
+    public message: string = '';
+    public result: any;
+    public records: number = 0;
+
+}
