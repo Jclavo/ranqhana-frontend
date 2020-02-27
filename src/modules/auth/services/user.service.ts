cd73a9d0e507cdac91a256d722cd41accfa71119
Jclavo <jclavotafur@gmail.com>
2020-02-27 19:32:23 -0300

    login function

--- src/modules/auth/services/user.service.ts
+++ src/modules/auth/services/user.service.ts
@@ -1,26 +1,70 @@
 import { Injectable } from '@angular/core';
-import { Observable, ReplaySubject } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from "rxjs/operators";
 
+//MODEL
 import { User } from '../models';
-
-const userSubject: ReplaySubject<User> = new ReplaySubject(1);
+import { Response } from '../../utility/models';
+// const userSubject: ReplaySubject<User> = new ReplaySubject(1);
 
 @Injectable()
 export class UserService {
-    constructor() {
-        this.user = {
-            id: '123',
-            firstName: 'Start',
-            lastName: 'Bootstrap',
-            email: 'no-reply@startbootstrap.com',
-        };
+
+    private apiRoot: string = 'https://blooming-hamlet-62206.herokuapp.com/api/';
+
+    constructor(private http: HttpClient) {
+        // this.user = {
+        //     id: '123',
+        //     firstName: 'Start',
+        //     lastName: 'Bootstrap',
+        //     email: 'no-reply@startbootstrap.com',
+        // };
     }
 
-    set user(user: User) {
-        userSubject.next(user);
-    }
+    // set user(user: User) {
+    //     userSubject.next(user);
+    // }
 
-    get user$(): Observable<User> {
-        return userSubject.asObservable();
-    }
+    // get user$(): Observable<User> {
+    //     return userSubject.asObservable();
+    // }
+
+    login(user: User): Observable<Response> {
+
+        let apiRoot = this.apiRoot + 'login';
+    
+        return this.http.post(apiRoot, user).pipe(map(res => {
+    
+          let response = new Response();
+          let resultRAW: any = res;
+    
+          //Set response
+          response.status = resultRAW.status;
+          response.message = resultRAW.message;
+    
+          if (resultRAW.result) {
+            let user = new User();
+            user.id = resultRAW.result?.id;
+            user.name = resultRAW.result?.name;
+            user.email = resultRAW.result?.email;
+            user.identification = resultRAW.result?.identification;
+            user.country_code = resultRAW.result?.country_code;
+            user.api_token = resultRAW.result?.api_token;
+            user.store_id = resultRAW.result?.store_id;
+    
+            response.result = user;
+            //response.records = resultRAW.result?.length;
+    
+          }
+    
+          return response;
+    
+        }),
+          catchError(error => {
+            return throwError(error.message);
+          }));
+      }
+
+
 }
