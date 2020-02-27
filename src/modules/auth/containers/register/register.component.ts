faf7b741d77729249f6fca7175d2f60dfbff780e
Jclavo <jclavotafur@gmail.com>
2020-02-27 20:30:14 -0300

    add register module

--- src/modules/auth/containers/register/register.component.ts
+++ src/modules/auth/containers/register/register.component.ts
@@ -1,4 +1,16 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+// MODELS
+import { User } from '../../models/user.model';
+import { Country } from '../../../country/models';
+import { Response } from '../../../utility/models/index';
+
+// SERVICE
+import { UtilityService, NotificationService } from '../../../utility/services';
+import { UserService } from '../../services';
+import { CountryService } from '../../../country/services';
 
 @Component({
     selector: 'sb-register',
@@ -7,6 +19,114 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
     styleUrls: ['register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-    constructor() {}
-    ngOnInit() {}
+
+    public countries: Array<Country> = [];
+
+    public errorsList: Array<string> = [];
+
+    public userRegister = new User();
+
+    registerForm: FormGroup = this.fb.group({
+        country_code: ['', [Validators.required]],
+        name: ['', [Validators.required]],
+        identification: ['', [Validators.required]],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required]],
+        c_password: ['', [Validators.required]],
+        store_id: ['1', [Validators.required]]
+    },
+        {
+            validator: this.MustMatch('password', 'c_password')
+        });
+
+
+    constructor(
+        private fb: FormBuilder,
+        private utilityService: UtilityService,
+        private notificationService: NotificationService,
+        private userService: UserService,
+        private countryService: CountryService,
+        private router: Router
+    ) {
+        this.getAllCountries();
+
+    }
+
+    ngOnInit() { }
+
+    getAllCountries() {
+        this.countryService.getAll().subscribe((response: Response) => {
+
+            if (response.status)
+                this.countries = response.result;
+
+        }, error => {
+            this.notificationService.error(error);
+        });
+    }
+
+    getFormValues() {
+
+        let userRegister = new User();
+
+        userRegister.country_code = this.registerForm.value.country_code;
+        userRegister.name = this.registerForm.value.identification;
+        userRegister.email = this.registerForm.value.email;
+        userRegister.identification = this.registerForm.value.identification;
+        userRegister.password = this.registerForm.value.password;
+        userRegister.c_password = this.registerForm.value.c_password;
+        userRegister.store_id = this.registerForm.value.store_id;
+
+        console.log('userRegister', userRegister);
+
+        return userRegister;
+    }
+
+    signUp() {
+        console.log('register...');
+    }
+
+    register() {
+
+        if (this.registerForm.invalid) {
+            this.errorsList = this.utilityService.getFormError(this.registerForm);
+            this.notificationService.error(this.errorsList[0]);
+        }
+
+        this.userRegister = this.getFormValues();
+
+        this.userService.register(this.userRegister).subscribe((response: Response) => {
+
+            if (response.status) {
+                this.notificationService.success(response.message);
+                this.router.navigate(['/login']);
+              }
+              else {
+                this.notificationService.error(response.message);
+              }
+
+        }, (error: any) => {
+            console.log(error);
+            this.notificationService.error(error);
+          });
+    }
+
+    MustMatch(controlName: string, matchingControlName: string) {
+        return (formGroup: FormGroup) => {
+            const control = formGroup.controls[controlName];
+            const matchingControl = formGroup.controls[matchingControlName];
+
+            if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+                // return if another validator has already found an error on the matchingControl
+                return;
+            }
+
+            // set error on matchingControl if validation fails
+            if (control.value !== matchingControl.value) {
+                matchingControl.setErrors({ mustMatch: true });
+            } else {
+                matchingControl.setErrors(null);
+            }
+        }
+    }
 }
--- src/modules/auth/services/user.service.ts
+++ src/modules/auth/services/user.service.ts
@@ -11,60 +11,81 @@ import { Response } from '../../utility/models';
 @Injectable()
 export class UserService {
 
-    private apiRoot: string = 'https://blooming-hamlet-62206.herokuapp.com/api/';
+  private apiRoot: string = 'https://blooming-hamlet-62206.herokuapp.com/api/';
 
-    constructor(private http: HttpClient) {
-        // this.user = {
-        //     id: '123',
-        //     firstName: 'Start',
-        //     lastName: 'Bootstrap',
-        //     email: 'no-reply@startbootstrap.com',
-        // };
-    }
+  constructor(private http: HttpClient) {
+    // this.user = {
+    //     id: '123',
+    //     firstName: 'Start',
+    //     lastName: 'Bootstrap',
+    //     email: 'no-reply@startbootstrap.com',
+    // };
+  }
 
-    // set user(user: User) {
-    //     userSubject.next(user);
-    // }
+  // set user(user: User) {
+  //     userSubject.next(user);
+  // }
 
-    // get user$(): Observable<User> {
-    //     return userSubject.asObservable();
-    // }
+  // get user$(): Observable<User> {
+  //     return userSubject.asObservable();
+  // }
 
-    login(user: User): Observable<Response> {
+  login(user: User): Observable<Response> {
 
-        let apiRoot = this.apiRoot + 'login';
-    
-        return this.http.post(apiRoot, user).pipe(map(res => {
-    
-          let response = new Response();
-          let resultRAW: any = res;
-    
-          //Set response
-          response.status = resultRAW.status;
-          response.message = resultRAW.message;
-    
-          if (resultRAW.result) {
-            let user = new User();
-            user.id = resultRAW.result?.id;
-            user.name = resultRAW.result?.name;
-            user.email = resultRAW.result?.email;
-            user.identification = resultRAW.result?.identification;
-            user.country_code = resultRAW.result?.country_code;
-            user.api_token = resultRAW.result?.api_token;
-            user.store_id = resultRAW.result?.store_id;
-    
-            response.result = user;
-            //response.records = resultRAW.result?.length;
-    
-          }
-    
-          return response;
-    
-        }),
-          catchError(error => {
-            return throwError(error.message);
-          }));
+    let apiRoot = this.apiRoot + 'login';
+
+    return this.http.post(apiRoot, user).pipe(map(res => {
+
+      let response = new Response();
+      let resultRAW: any = res;
+
+      //Set response
+      response.status = resultRAW.status;
+      response.message = resultRAW.message;
+
+      if (resultRAW.result) {
+        let user = new User();
+        user.id = resultRAW.result?.id;
+        user.name = resultRAW.result?.name;
+        user.email = resultRAW.result?.email;
+        user.identification = resultRAW.result?.identification;
+        user.country_code = resultRAW.result?.country_code;
+        user.api_token = resultRAW.result?.api_token;
+        user.store_id = resultRAW.result?.store_id;
+
+        response.result = user;
+        //response.records = resultRAW.result?.length;
+
       }
+
+      return response;
+
+    }),
+      catchError(error => {
+        return throwError(error.message);
+      }));
+  }
+
+  register(user: User): Observable<Response> {
+
+    let apiRoot = this.apiRoot + 'register';
+    return this.http.post(apiRoot, user).pipe(map(res => {
+
+      let response = new Response();
+      let resultRAW: any = res;
+
+      //Set response
+      response.status = resultRAW.status;
+      response.message = resultRAW.message;
+      // response.result = resultRAW.result;
+      // response.records = resultRAW.result.length;
+      return response;
+
+    }),
+      catchError(error => {
+        return throwError(error.message);
+      }));
+  }
 
 
 }
