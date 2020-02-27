241fad2cb95d3ee6fa9d2ce1d71e013a508129ea
Jclavo <jclavotafur@gmail.com>
2020-02-27 19:31:47 -0300

    add user model

--- src/modules/auth/models/auth.model.ts
+++ src/modules/auth/models/auth.model.ts
@@ -1,6 +1,6 @@
-export interface User {
-    id: string;
-    firstName: string;
-    lastName: string;
-    email: string;
-}
+// export interface User {
+//     id: string;
+//     firstName: string;
+//     lastName: string;
+//     email: string;
+// }
---
+++ src/modules/auth/models/user.model.ts
@@ -0,0 +1,11 @@
+export class User {
+    id: string = '';
+    name: string = '';
+    email: string = '';
+    identification: string = '';
+    password: string = '';
+    c_password: string = '';
+    country_code: string = '';
+    api_token: string = '';
+    store_id: string = '';
+}
