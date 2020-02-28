f30d945c065b642052f7bba4bbaec81273ed1041
Jclavo <jclavotafur@gmail.com>
2020-02-28 20:21:26 -0300

    delete product component

--- src/modules/products/containers/index.ts
+++
@@ -1,8 +0,0 @@
-// import { nameComponent } from './folder/name.component';
-// import { anotherComponent } from './folder/another.component';
-
-// export const containers = [nameComponent, anotherComponent];
-
-// export * from './folder/name.component';
-// export * from './folder/another.component';
-
--- src/modules/products/services/index.ts
+++
@@ -1,7 +0,0 @@
-// import { NameService } from './name.service';
-// import { AnotherService } from './another.service';
-
-// export const services = [NameService, AnotherService];
-
-// export * from './name.service';
-// export * from './another.service';
