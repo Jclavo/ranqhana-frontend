7c3b9e4ce0d159b30772254ea0c5c75e1d35f7c4
Jclavo <jclavotafur@gmail.com>
2020-02-27 21:43:52 -0300

    create product component structure

---
+++ src/modules/products/containers/index.ts
@@ -0,0 +1,8 @@
+// import { nameComponent } from './folder/name.component';
+// import { anotherComponent } from './folder/another.component';
+
+// export const containers = [nameComponent, anotherComponent];
+
+// export * from './folder/name.component';
+// export * from './folder/another.component';
+
---
+++ src/modules/products/services/index.ts
@@ -0,0 +1,7 @@
+// import { NameService } from './name.service';
+// import { AnotherService } from './another.service';
+
+// export const services = [NameService, AnotherService];
+
+// export * from './name.service';
+// export * from './another.service';
