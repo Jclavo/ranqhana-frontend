92cb6b55046e120be3c59732f282e423a56bedc4
Jclavo <jclavotafur@gmail.com>
2020-02-29 20:30:40 -0300

    backup from items-list

---
+++ src/modules/items copy/containers/index.ts
@@ -0,0 +1,8 @@
+import { ItemListComponent } from './item-list/item-list.component';
+// import { anotherComponent } from './folder/another.component';
+
+export const containers = [ItemListComponent];
+
+export * from './item-list/item-list.component';
+// export * from './folder/another.component';
+
