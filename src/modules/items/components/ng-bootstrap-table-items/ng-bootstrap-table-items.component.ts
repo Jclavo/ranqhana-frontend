fab182d43889a82dc84a5be811ee3b6d5a2db646
Jclavo <jclavotafur@gmail.com>
2020-03-05 23:09:55 -0300

    set pagination for items

--- src/modules/items/components/ng-bootstrap-table-items/ng-bootstrap-table-items.component.ts
+++ src/modules/items/components/ng-bootstrap-table-items/ng-bootstrap-table-items.component.ts
@@ -1,46 +1,113 @@
-import { Component, OnInit, Input, ViewChildren, QueryList, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, Input, ViewChildren, QueryList, ChangeDetectionStrategy, ChangeDetectorRef, KeyValueDiffer, KeyValueDiffers } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { SBSortableHeaderDirective, SortEvent } from '@modules/items/directives';
 import { ItemService } from '@modules/items/services';
-import { Item } from '@modules/items/models';
+
+//MODELS
+import { Item, SearchOptions } from '@modules/items/models';
+
 
 @Component({
   selector: 'sb-ng-bootstrap-table-items',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  // changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './ng-bootstrap-table-items.component.html',
   styleUrls: ['./ng-bootstrap-table-items.component.scss']
 })
 export class NgBootstrapTableItemsComponent implements OnInit {
 
-  @Input() pageSize = 4;
+  public searchOption = new SearchOptions();
+  public items: Array<Item> = [];
+  public parameters: any;
 
-  items$!: Observable<Item[]>;
-  total$!: Observable<number>;
-  sortedColumn!: string;
-  sortedDirection!: string;
+  private searchOptionDiffers: KeyValueDiffer<string, any>;
+
+  public sortedColumn!: string;
+  public sortedDirection!: string;
+
+  // @Input() pageSize = 2;
+
+  // items$!: Observable<Item[]>;
+  // total$!: Observable<number>;
+  
 
   @ViewChildren(SBSortableHeaderDirective) headers!: QueryList<SBSortableHeaderDirective>;
 
   constructor(
     private itemService: ItemService,
-    private changeDetectorRef: ChangeDetectorRef
-  ) { }
+    private changeDetectorRef: ChangeDetectorRef,
+    private differs: KeyValueDiffers
+  ) { 
+
+    // this.customer = new Customer();
+    this.searchOptionDiffers = this.differs.find(this.searchOption).create();
+  }
 
   ngOnInit(): void {
 
     // console.log('this.items$',this.items$);
-    this.itemService.pageSize = this.pageSize;
-    this.items$ = this.itemService.items$;
-    this.total$ = this.itemService.total$;
+    // this.itemService.pageSize = this.pageSize;
+    // this.items$ = this.itemService.items$;
+    // this.total$ = this.itemService.total$;
+
+    // this.searchOption.pageSize = this.pageSize;
+
+    // this.searchOption.pageSize = this.pageSize;
+    console.log('this.searchOption', this.searchOption);
+
+    this.getItems();
+
+  }
+
+  ngDoCheck(): void {
+    const changes = this.searchOptionDiffers.diff(this.searchOption);
+    if (changes) {
+      console.log('changed');
+      this.getItems();
+    }
   }
 
   onSort({ column, direction }: SortEvent) {
     this.sortedColumn = column;
     this.sortedDirection = direction;
-    this.itemService.sortColumn = column;
-    this.itemService.sortDirection = direction;
-    this.changeDetectorRef.detectChanges();
+
+    this.searchOption.sortColumn = this.sortedColumn;
+    this.searchOption.sortDirection = this.sortedDirection;
+    // this.itemService.sortColumn = column;
+    // this.itemService.sortDirection = direction;
+    // this.changeDetectorRef.detectChanges();
+    // console.log('this.searchOption', this.searchOption);
   }
+
+
+  getItems() {
+    let parameters = { 'store_id' : 1, 'searchOption': this.searchOption};
+
+    this.itemService.getAll(parameters).subscribe(response => {
+
+      console.log(response.message);
+      if (response.status) {
+
+        this.items = response.result;
+
+        this.searchOption.total = response.records;
+
+        console.log('this.searchOption', this.searchOption);
+
+        // this.changeDetectorRef.detectChanges();
+
+
+      }
+
+    }, error => {
+      console.log(error);
+      //loading.dismiss();
+    });
+
+  }
+
+
+
+
 
 }
--- src/modules/items/services/item.service.ts
+++ src/modules/items/services/item.service.ts
@@ -1,132 +1,39 @@
-import { Injectable, PipeTransform } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, throwError, BehaviorSubject, Subject } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { map, catchError, switchMap } from "rxjs/operators";
-import { SortDirection } from "@modules/items/directives";
-import { DecimalPipe } from '@angular/common';
 
 //Models
-import { Item } from '@modules/items/models';
+import { Item, SearchOptions } from '@modules/items/models';
 import { Response } from '@modules/utility/models';
 
-interface State {
-  page: number;
-  pageSize: number;
-  searchTerm: string;
-  sortColumn: string;
-  sortDirection: SortDirection;
-}
-
-function compare(v1: number | string, v2: number | string) {
-  return v1 < v2 ? -1 : v1 > v2 ? 1 : 0;
-}
-
-function sort(items: Item[], column: string, direction: string): Item[] {
-  if (direction === '') {
-    return items;
-  } else {
-    return [...items].sort((a, b) => {
-      const res = compare(a[column], b[column]);
-      return direction === 'asc' ? res : -res;
-    });
-  }
-}
-
-function matches(item: Item, term: string, pipe: PipeTransform) {
-  return (
-    item.name.toLowerCase().includes(term.toLowerCase()) ||
-    item.description?.toLowerCase().includes(term.toLowerCase()) ||
-    pipe.transform(item.quantity).includes(term)
-  );
-}
+//SERVICES
+import { AuthService } from '@modules/auth/services';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
 
-  private _items$ = new BehaviorSubject<Item[]>([]);
-  private _search$ = new Subject<void>();
-  private _total$ = new BehaviorSubject<number>(0);
-
-  private _state: State = {
-    page: 1,
-    pageSize: 4,
-    searchTerm: '',
-    sortColumn: '',
-    sortDirection: '',
-  };
-
   private apiRoot: string = 'https://blooming-hamlet-62206.herokuapp.com/api/';
 
   private httpOptions = {
     headers: new HttpHeaders({
       //'Content-Type':  'application/json',
-      // 'Authorization': 'Bearer ' + `this.authService.getAPITOKEN()`
-      'Authorization': 'Bearer ' + 'DwFCPuQJ9UjAXJ4l8PdHr3PYPfM39JOiCzywD3Wtq5yEX3DoptAcu2pClMemIoTGlNnPSiQqTODYQhUO'
-    })
+      'Authorization': 'Bearer ' + this.authService.getAPITOKEN()
+      })
   };
 
   constructor(
     private http: HttpClient,
-    private pipe: DecimalPipe
-    // private authService: AuthService
-  ) {
-    this._search$
-      .pipe(
-        switchMap(() => this._getAll()),
-      )
-      .subscribe(result => {
-        this._items$.next(result.result);
-        console.log('result.result.length', result.records);
-        this._total$.next(result.records);
-      });
+    private authService: AuthService
+  ) {}
 
-    this._search$.next();
+  getAll(parameters: any): Observable<Response> {
 
-  }
+    let apiRoot = this.apiRoot + 'items/pagination?page=' + parameters.searchOption.page;
 
-  get items$() {
-    return this._items$.asObservable();
-  }
-  get total$() {
-    return this._total$.asObservable();
-  }
-  get page() {
-    return this._state.page;
-  }
-  set page(page: number) {
-    this._set({ page });
-  }
-  get pageSize() {
-    return this._state.pageSize;
-  }
-  set pageSize(pageSize: number) {
-    this._set({ pageSize });
-  }
-  get searchTerm() {
-    return this._state.searchTerm;
-  }
-  set searchTerm(searchTerm: string) {
-    this._set({ searchTerm });
-  }
-  set sortColumn(sortColumn: string) {
-    this._set({ sortColumn });
-  }
-  set sortDirection(sortDirection: SortDirection) {
-    this._set({ sortDirection });
-  }
-
-  private _set(patch: Partial<State>) {
-    Object.assign(this._state, patch);
-    this._search$.next();
-  }
-
-  _getAll(): Observable<Response> {
-
-    let apiRoot = this.apiRoot + 'products';
-
-    return this.http.get(apiRoot, this.httpOptions).pipe(map(res => {
+    return this.http.post(apiRoot, parameters, this.httpOptions).pipe(map(res => {
 
       let response = new Response();
       let resultRAW: any = res;
@@ -149,22 +56,7 @@ export class ItemService {
         return item;
       });
 
-      response.records = resultRAW.result?.length;
-
-
-      const { sortColumn, sortDirection, pageSize, page, searchTerm } = this._state;
-
-      // 1. sort
-      let items = sort(response.result, sortColumn, sortDirection);
-
-      // 2. filter
-      items = items.filter(item => matches(item, searchTerm, this.pipe));
-      response.records = items.length;
-
-      // 3. paginate
-      items = items.slice((page - 1) * pageSize, (page - 1) * pageSize + pageSize);
-
-      response.result = items;
+      response.records = resultRAW.records;
 
       return response;
 
