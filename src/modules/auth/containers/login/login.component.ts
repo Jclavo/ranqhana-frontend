0a12ebee2d00c54cfb257e044fd96916482df806
Jclavo <jclavotafur@gmail.com>
2020-03-09 22:38:04 -0300

    clean storage to load page

--- src/modules/auth/containers/login/login.component.ts
+++ src/modules/auth/containers/login/login.component.ts
@@ -42,6 +42,7 @@ export class LoginComponent implements OnInit {
     private countryService: CountryService,
     private authService: AuthService,
   ) {
+    this.authService.cleanStorage();
     this.getAllCountries();
   }
 
