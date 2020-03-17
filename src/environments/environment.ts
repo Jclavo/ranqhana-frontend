ec3023f83cf0be490b4c3204a5d5fe9e2bfc5916
Jclavo <jclavotafur@gmail.com>
2020-03-17 20:33:25 -0300

    add informative texts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
     production: false,
-    apiURL: 'https://blooming-hamlet-62206.herokuapp.com/api/',
-    // apiURL: 'http://127.0.0.1:8000/api/',
+    //apiURL: 'https://blooming-hamlet-62206.herokuapp.com/api/',
+    apiURL: 'http://127.0.0.1:8000/api/',
 };
 
 /*
