65b5166c229ad31e3df4b0187c93797fc02af3a4
Jclavo <jclavotafur@gmail.com>
2020-04-01 21:01:42 -0300

    add template from sort-icons

--- src/modules/utility/components/index.ts
+++ src/modules/utility/components/index.ts
@@ -1,5 +1,7 @@
 import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
+import { SortIconComponent } from './sort-icon/sort-icon.component';
 
-export const components = [ConfirmModalComponent];
+export const components = [ConfirmModalComponent, SortIconComponent];
 
 export * from './confirm-modal/confirm-modal.component';
+export * from './sort-icon/sort-icon.component';
