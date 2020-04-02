da898e2afeb144d1db5577b73caa1fd099740336
Jclavo <jclavotafur@gmail.com>
2020-04-01 21:01:15 -0300

    delete sort icon

--- src/modules/invoices/components/index.ts
+++ src/modules/invoices/components/index.ts
@@ -1,9 +1,9 @@
 import { AddAditionalInfoComponent } from './add-aditional-info/add-aditional-info.component';
 import { NgBootstrapTableInvoicesComponent } from './ng-bootstrap-table-invoices/ng-bootstrap-table-invoices.component';
-import { SortIconComponent } from './sort-icon/sort-icon.component';
+// import { SortIconComponent } from './sort-icon/sort-icon.component';
 
-export const components = [AddAditionalInfoComponent, NgBootstrapTableInvoicesComponent, SortIconComponent ];
+export const components = [AddAditionalInfoComponent, NgBootstrapTableInvoicesComponent ];
 
 export * from './add-aditional-info/add-aditional-info.component';
 export * from './ng-bootstrap-table-invoices/ng-bootstrap-table-invoices.component';
-export * from './sort-icon/sort-icon.component';
+// export * from './sort-icon/sort-icon.component';
