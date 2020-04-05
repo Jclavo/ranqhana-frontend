0f5d09a0570a1d97af79d83514fcbb621b74c00c
Jclavo <jclavotafur@gmail.com>
2020-04-05 14:19:47 -0300

    add unit

--- src/modules/invoices/models/InvoiceDetail.model.ts
+++ src/modules/invoices/models/InvoiceDetail.model.ts
@@ -1,7 +1,8 @@
 export class InvoiceDetail {
     id: string = '';
-    name: string = '';
     item_id: string = '';
+    item: string = '';
+    unit: string = '';
     quantity:  number = 0.0;
     price: number = 0.0;
     total: number = 0.0;   
