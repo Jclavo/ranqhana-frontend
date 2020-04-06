02ff04ce0cec8287a6713abc76de6c9e20f0c3d8
Jclavo <jclavotafur@gmail.com>
2020-04-06 18:21:53 -0300

    add type of invoice to the list

--- src/modules/invoices/models/SearchInvoice.model.ts
+++ src/modules/invoices/models/SearchInvoice.model.ts
@@ -2,21 +2,19 @@ import { SearchOptions } from "@modules/utility/models";
 
 export class SearchInvoice extends SearchOptions {
 
-
-    fromDate: string = "";
-    toDate: string = "";
+    fromDate: string = '';
+    toDate: string = '';
+    type: string = 'S';
 
     constructor() {
         super();
-        // this.fromDate = this.getCurrentDate();
-        // this.toDate = this.getCurrentDate();
     }
 
-    getCurrentDate() {
-        return new Date().getFullYear() + "-" +
-            (((new Date().getMonth() + 1) < 10) ? "0" : "") + (new Date().getMonth() + 1) + "-" +
-            ((new Date().getDate() < 10) ? "0" : "") + new Date().getDate() ;
-    }
+    // getCurrentDate() {
+    //     return new Date().getFullYear() + "-" +
+    //         (((new Date().getMonth() + 1) < 10) ? "0" : "") + (new Date().getMonth() + 1) + "-" +
+    //         ((new Date().getDate() < 10) ? "0" : "") + new Date().getDate() ;
+    // }
 
 
 }
