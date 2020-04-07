5f51444c3040cd2fc44f228b9489f2166f3a1077
Jclavo <jclavotafur@gmail.com>
2020-04-06 21:37:56 -0300

    add icon check circle

--- src/modules/icons/icons.font-awesome-solid.ts
+++ src/modules/icons/icons.font-awesome-solid.ts
@@ -16,6 +16,7 @@ import {
     faTable,
     faTachometerAlt,
     faUser,
+    faCheckCircle
 } from '@fortawesome/free-solid-svg-icons';
 
 export const fontAwesomeSolidIcons = {
@@ -34,4 +35,5 @@ export const fontAwesomeSolidIcons = {
     faTable,
     faTachometerAlt,
     faUser,
+    faCheckCircle
 };
