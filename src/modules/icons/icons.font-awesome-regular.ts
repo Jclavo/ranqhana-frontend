1c850e0a6e790093571e77b785cda8209c39c84b
Jclavo <jclavotafur@gmail.com>
2020-04-07 21:43:50 -0300

    add icon show

--- src/modules/icons/icons.font-awesome-regular.ts
+++ src/modules/icons/icons.font-awesome-regular.ts
@@ -1,7 +1,7 @@
 /* All free solid fontawesome icons -> https://fontawesome.com/icons?d=gallery&s=regular&m=free */
 
-import { faBell, faTrashAlt, faEdit } from '@fortawesome/free-regular-svg-icons';
+import { faBell, faTrashAlt, faEdit, faEye } from '@fortawesome/free-regular-svg-icons';
 
 export const fontAwesomeRegularIcons = {
-    faBell, faTrashAlt, faEdit
+    faBell, faTrashAlt, faEdit, faEye
 };
