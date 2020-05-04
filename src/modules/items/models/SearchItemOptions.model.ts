588de52243639f32d6fbd88419560061f0e9e994
Jclavo <jclavotafur@gmail.com>
2020-05-04 20:46:05 -0300

    create searchItemOptions

---
+++ src/modules/items/models/SearchItemOptions.model.ts
@@ -0,0 +1,11 @@
+import { SearchOptions } from "@modules/utility/models";
+
+export class SearchItemOptions extends SearchOptions {
+
+    invoice_type_id: number = 0;
+
+    constructor() {
+        super();
+    }
+
+}
