793e3c52a422944bfcdfe3fd7e4810ee3ee2832a
Jclavo <jclavotafur@gmail.com>
2020-05-06 17:43:51 -0300

    add model to graphic

---
+++ src/modules/utility/models/graphic.model.ts
@@ -0,0 +1,7 @@
+export class Graphic {
+
+    X: string = '';
+    Y: number = 0;
+    // labelX: string = '';
+    // labelY: string = '';
+}
--- src/modules/utility/models/index.ts
+++ src/modules/utility/models/index.ts
@@ -1,3 +1,4 @@
 export * from './utility.model';
 export * from './response.model';
 export * from './search.model';
+export * from './graphic.model';
