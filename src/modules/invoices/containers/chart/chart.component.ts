e049b5e24bf508d1e965a455cc8dcc672edb4ad7
Jclavo <jclavotafur@gmail.com>
2020-05-06 19:49:59 -0300

    show graphic for invoice

--- src/modules/invoices/containers/chart/chart.component.ts
+++ src/modules/invoices/containers/chart/chart.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, KeyValueDiffer, KeyValueDiffers } from '@angular/core';
+
+//SERVICES
+import { InvoiceService } from "../../services/invoice.service";
+import { NotificationService, CustomDateService } from '@modules/utility/services';
+import { AuthService } from "@modules/auth/services";
+
+//MODEL
+import { SearchInvoice } from "../../models";
+import { Graphic } from '@modules/utility/models';
 
 @Component({
   selector: 'sb-chart',
@@ -7,9 +16,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartComponent implements OnInit {
 
-  constructor() { }
+  public searchInvoiceOption = new SearchInvoice();
+  public graphicData: Array<Graphic> = [];
+
+  constructor(
+    private invoiceService: InvoiceService,
+    private notificationService: NotificationService,
+    private authService: AuthService,
+    private customDateService: CustomDateService
+    ) { 
+      this.searchInvoiceOption.fromDate = this.searchInvoiceOption.toDate = this.customDateService.getToday();
+    }
 
   ngOnInit(): void {
+  }
+
+
+  search(){
+
+    this.invoiceService.report(this.searchInvoiceOption).subscribe(response => {
+
+      if (response.status) {
+        this.graphicData = response.result;
+        // console.log('this.graphicData', this.graphicData);
+
+      }else{
+        this.notificationService.error(response.message);
+      }
+
+    }, error => {
+      this.notificationService.error(error);
+      this.authService.raiseError();
+    });
   }
 
 }
