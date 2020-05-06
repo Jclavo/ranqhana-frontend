5add87bf727dee2a48619efcdf9e640b4e2304c2
Jclavo <jclavotafur@gmail.com>
2020-05-06 19:48:43 -0300

    add searchBy property to invoiceModel

--- src/modules/invoices/models/SearchInvoice.model.ts
+++ src/modules/invoices/models/SearchInvoice.model.ts
@@ -5,16 +5,9 @@ export class SearchInvoice extends SearchOptions {
     fromDate: string = '';
     toDate: string = '';
     type_id: number = 1;
+    searchBy: string = 'D';
 
     constructor() {
         super();
     }
-
-    // getCurrentDate() {
-    //     return new Date().getFullYear() + "-" +
-    //         (((new Date().getMonth() + 1) < 10) ? "0" : "") + (new Date().getMonth() + 1) + "-" +
-    //         ((new Date().getDate() < 10) ? "0" : "") + new Date().getDate() ;
-    // }
-
-
 }
