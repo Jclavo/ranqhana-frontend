c7d5b4c2776cfd927a768d78e14212f5609d7465
Jclavo <jclavotafur@gmail.com>
2020-05-17 17:10:19 -0300

    add pageSize to replace per_page

--- src/modules/utility/models/search.model.ts
+++ src/modules/utility/models/search.model.ts
@@ -1,6 +1,7 @@
 export class SearchOptions {
     page: number = 1;
     per_page: number = 20;
+    pageSize: number = 20;
     searchValue: string = '';
     total: number = 0;
     sortColumn: string = '';
