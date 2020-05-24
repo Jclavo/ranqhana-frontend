b7f585060d938529cc9e1014d5a7a590aae83b87
Jclavo <jclavotafur@gmail.com>
2020-05-24 13:25:29 -0300

    show login of user connected

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Router } from '@angular/router';
 //MODEL
-import { User } from '../models';
+import { User } from '@modules/users/models';
 import { HttpHeaders } from '@angular/common/http';
 
 @Injectable()
@@ -19,13 +19,13 @@ export class AuthService {
 
         this.setAPITOKEN(userInfo.api_token);
         // this.api_token = userInfo.api_token;
-
         this.user.id = userInfo.id;
         this.user.name = userInfo.name;
         this.user.email = userInfo.email;
         this.user.identification = userInfo.identification;
-        this.user.country_code = userInfo.country_code;
+        this.user.countryCode = userInfo.countryCode;
         this.user.store_id = userInfo.store_id;
+        this.user.login = userInfo.login;
 
         localStorage.setItem('user', JSON.stringify(this.user));
     }
@@ -46,6 +46,10 @@ export class AuthService {
 
     public getUserEmail() {
         return this.getUserFeature('email');
+    }
+
+    public getLogin() {
+        return this.getUserFeature('login');
     }
 
     public getUserStoreID() {
@@ -70,6 +74,8 @@ export class AuthService {
                     return JSON.parse(user)?.store_id;
                 case 'email':
                     return JSON.parse(user)?.email;
+                case 'login':
+                    return JSON.parse(user)?.login;
                 default:
                     return;
             }
