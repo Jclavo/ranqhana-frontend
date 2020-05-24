bcb87efb0f0d5dc73a5a339595f7d35d83efe83d
Jclavo <jclavotafur@gmail.com>
2020-05-24 15:51:59 -0300

    add fields about country for store model

--- src/modules/stores/models/store.model.ts
+++ src/modules/stores/models/store.model.ts
@@ -1,6 +1,8 @@
 export class Store {
-    id: string = '';
+    id: number = 0;
     name: string = '';
     code: string = '';
     country: string = '';
+    country_id: number = 0;
+    countryCode: string = '';
 }
