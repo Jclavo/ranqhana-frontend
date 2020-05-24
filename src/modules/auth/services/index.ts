c3188c2a88a74eccae59777e85673dfeb955aeb8
Jclavo <jclavotafur@gmail.com>
2020-05-24 13:24:27 -0300

    delete userservice from authmodule

--- src/modules/auth/services/index.ts
+++ src/modules/auth/services/index.ts
@@ -1,7 +1,6 @@
 import { AuthService } from './auth.service';
-import { UserService } from './user.service';
 
-export const services = [AuthService, UserService];
+export const services = [AuthService];
 
 export * from './auth.service';
-export * from './user.service';
+
