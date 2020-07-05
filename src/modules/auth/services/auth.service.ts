9f91f12fe6ea4e45f5f60e824d3b65bf435ed77b
Jclavo <jclavotafur@gmail.com>
2020-07-05 13:59:04 -0300

    add new fields for User and set them

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -15,17 +15,37 @@ export class AuthService {
         private router: Router,
     ) { }
 
-    public setUser(userInfo: User) {
+    public setUser(_user: User) {
 
-        this.setAPITOKEN(userInfo.api_token);
-        // this.api_token = userInfo.api_token;
-        this.user.id = userInfo.id;
-        this.user.name = userInfo.name;
-        this.user.email = userInfo.email;
-        this.user.identification = userInfo.identification;
-        this.user.countryCode = userInfo.countryCode;
-        this.user.store_id = userInfo.store_id;
-        this.user.login = userInfo.login;
+        this.setAPITOKEN(_user.api_token);
+
+        this.user.id = _user.id;
+        this.user.login = _user.login;
+        
+        //user info
+        // this.user.identification = _user.id;
+        // this.user.name = _user.id;
+        // this.user.lastname = _user.id;
+        // this.user.email = _user.id;
+        // this.user.phone = _user.id;
+        // this.user.address = _user.id;
+       
+        //company info
+        this.user.company_id = _user.company_id;
+        this.user.company = _user.company;
+    
+        //project info
+        this.user.project_id = _user.project_id;
+        this.user.project =  _user.project;
+        //country info
+        this.user.country_id = _user.country_id;
+        this.user.country = _user.country;
+        this.user.countryCode = _user.countryCode;
+        this.user.currency = _user.currency;
+        
+        //FK
+        this.user.company_project_id = _user.company_project_id;
+        this.user.user_detail_id = _user.user_detail_id;
 
         localStorage.setItem('user', JSON.stringify(this.user));
     }
@@ -56,6 +76,18 @@ export class AuthService {
         return this.getUserFeature('store_id');
     }
 
+    public getUserCountryCode() {
+        return this.getUserFeature('countryCode');
+    }
+
+    public getUserCompanyID() {
+        return this.getUserFeature('company_id');
+    }
+
+    public getUserProjectID() {
+        return this.getUserFeature('project_id');
+    }
+
     public getHeaders() {
         return {
           headers: new HttpHeaders({
@@ -76,6 +108,12 @@ export class AuthService {
                     return JSON.parse(user)?.email;
                 case 'login':
                     return JSON.parse(user)?.login;
+                case 'countryCode':
+                    return JSON.parse(user)?.countryCode;
+                case 'company_id':
+                    return JSON.parse(user)?.company_id;
+                case 'project_id':
+                    return JSON.parse(user)?.project_id;
                 default:
                     return;
             }
--- src/modules/users/models/user.model.ts
+++ src/modules/users/models/user.model.ts
@@ -1,17 +1,34 @@
 export class User {
+
     id: number = 0;
     login: string = '';
+    api_token: string = '';
+    password: string = '';
+    repassword: string = '';
+
+    //user info
     identification: string = '';
     name: string = '';
     lastname: string = '';
     email: string = '';
     phone: string = '';
     address: string = '';
-    password: string = '';
-    repassword: string = '';
-    store_id: number = 0;
-    store: string = '';
-    countryCode: string = '';
+   
+    //company info
+    company_id: number = 0;
+    company: string = '';
 
-    api_token: string = '';
+    //project info
+    project_id: number = 0;
+    project: string = '';
+
+    //country info
+    country_id: number = 0;
+    country: string = '';
+    countryCode: string = '';
+    currency: string = '';
+    
+    //FK
+    company_project_id: number = 0;
+    user_detail_id: number = 0;
 }
