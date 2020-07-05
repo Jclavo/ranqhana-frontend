1f32a3509ea93bb840ecee9cc601eadcd83aaaa3
Jclavo <jclavotafur@gmail.com>
2020-07-05 13:55:19 -0300

    add url for RolesProject

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
     production: false,
+    apiURLTaapaq: 'http://127.0.0.1:8080/api/',
     //apiURL: 'https://blooming-hamlet-62206.herokuapp.com/api/',
     apiURL: 'http://127.0.0.1:8000/api/',
 };
