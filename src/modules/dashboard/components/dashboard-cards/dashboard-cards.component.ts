a4cecd538544d3e1f1f381469ed43ede0a31409c
Jclavo <jclavotafur@gmail.com>
2020-07-17 10:44:28 -0300

    show daily sale in dashboard using card view value

--- src/modules/dashboard/components/dashboard-cards/dashboard-cards.component.ts
+++ src/modules/dashboard/components/dashboard-cards/dashboard-cards.component.ts
@@ -1,12 +1,63 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+//SERVICES
+import { InvoiceService } from "@modules/invoices/services/invoice.service";
+import { NotificationService, CustomDateService } from '@modules/utility/services';
+import { AuthService } from "@modules/auth/services";
+
+//MODEL
+import { SearchInvoice } from "@modules/invoices/models";
+import { Graphic } from "@modules/utility/models";
 
 @Component({
     selector: 'sb-dashboard-cards',
-    changeDetection: ChangeDetectionStrategy.OnPush,
     templateUrl: './dashboard-cards.component.html',
     styleUrls: ['dashboard-cards.component.scss'],
 })
 export class DashboardCardsComponent implements OnInit {
-    constructor() {}
-    ngOnInit() {}
+
+    public searchInvoiceOption = new SearchInvoice();
+    public graphicData: Array<Graphic> = [];
+    // public dailySales: number = 0;
+    public dailySales = {value: 0};
+    
+
+    constructor(
+        private invoiceService: InvoiceService,
+        private notificationService: NotificationService,
+        private authService: AuthService,
+        private customDateService: CustomDateService
+    ) {
+
+        //set parameters to search
+        this.searchInvoiceOption.fromDate = this.searchInvoiceOption.toDate = this.customDateService.getToday();
+        this.searchInvoiceOption.searchBy = 'D';
+    }
+
+
+    ngOnInit(): void {
+        this.search();
+    }
+
+
+    search() {
+
+        this.invoiceService.report(this.searchInvoiceOption).subscribe(response => {
+
+            if (response.status) {
+                this.graphicData = response.result;
+                if(this.graphicData.length > 0){
+                    this.dailySales.value = this.graphicData[0]?.Y;
+                }
+                console.log('this.dailySales', this.dailySales);
+
+            } else {
+                this.notificationService.error(response.message);
+            }
+
+        }, error => {
+            this.notificationService.error(error);
+            this.authService.raiseError();
+        });
+    }
 }
