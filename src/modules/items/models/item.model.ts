2292e590942c9b5f23b3aa83aa75c61152c25b02
Jclavo <jclavotafur@gmail.com>
2020-07-30 19:33:57 -0300

    add field stocks to item model

--- src/modules/items/models/item.model.ts
+++ src/modules/items/models/item.model.ts
@@ -12,6 +12,8 @@ export class Item {
     store_id: number = 0;
     created_at: string = ''; 
     updated_at: string = ''; 
+
+    stocks: Array<number|string> = [];
     
     //extra fields
     quantity: number = 0;
