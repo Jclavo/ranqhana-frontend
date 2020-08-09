200e3b980ca76ef839add48aef75bbe66c0e575a
Jclavo <jclavotafur@gmail.com>
2020-08-09 19:28:10 -0300

    Item report shows the type (product/service)

--- src/modules/items/models/SearchItemOptions.model.ts
+++ src/modules/items/models/SearchItemOptions.model.ts
@@ -3,6 +3,7 @@ import { SearchOptions } from "@modules/utility/models";
 export class SearchItemOptions extends SearchOptions {
 
     stock_type_id: number = 0;
+    type_id: number = 0;
 
     constructor() {
         super();
