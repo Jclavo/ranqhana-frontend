2efbe83c4be07b2c14b37251eeb94e02b0474a22
Jclavo <jclavotafur@gmail.com>
2020-08-13 21:40:51 -0300

    delete meth SetType fr Purchase/sell invoice model

--- src/modules/invoices/models/PurchaseInvoice.model.ts
+++ src/modules/invoices/models/PurchaseInvoice.model.ts
@@ -4,7 +4,6 @@ export class PurchaseInvoice extends Invoice {
 
     constructor(){
         super();
-        this.setTypeForPurchase();
     }
      
 }
--- src/modules/invoices/models/SellInvoice.model.ts
+++ src/modules/invoices/models/SellInvoice.model.ts
@@ -4,7 +4,6 @@ export class SellInvoice extends Invoice {
 
     constructor(){
         super();
-        this.setTypeForSell();
     }
      
 }
