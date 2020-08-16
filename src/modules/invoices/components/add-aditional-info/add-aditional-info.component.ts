0b3833a34d55a0270171c4f695150391f40a6682
Jclavo <jclavotafur@gmail.com>
2020-08-16 18:03:45 -0300

    add client/company when invoice created

--- src/modules/invoices/components/add-aditional-info/add-aditional-info.component.ts
+++ src/modules/invoices/components/add-aditional-info/add-aditional-info.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap, map } from 'rxjs/operators';
 
 //MODELS
 import { SellInvoice, Invoice } from "../../models";
+import { User, SearchUserOptions } from "@modules/users/models";
+import { Role } from "@modules/roles/models";
 
 //SERVICES
 import { NotificationService } from '@modules/utility/services';
 import { AuthService } from "@modules/auth/services";
+import { UserService } from "@modules/users/services";
 import { InvoiceService } from '../../services';
 
 @Component({
@@ -19,35 +24,68 @@ export class AddAditionalInfoComponent implements OnInit {
   @Input() invoice_id: number = 0;
 
   public invoice = new Invoice();
-  
+  public searchUserOption = new SearchUserOptions();
+  public externalUser: any;
+
   constructor(
     public activeModal: NgbActiveModal,
     public notificationService: NotificationService,
     public invoiceService: InvoiceService,
     public authService: AuthService,
+    public userService: UserService,
   ) { }
- 
+
+
+  formatter = (user: User) => user.identification + ' - ' + user.name + ' ' + user.lastname;
+
+  search = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      switchMap(searchValue =>
+        this.getUsers(searchValue)
+      )
+    )
+
+  getUsers(searchValue: string) {
+
+    this.searchUserOption.searchValue = searchValue;
+    this.searchUserOption.role_id = Role.getClientID();
+
+    return this.userService.get(this.searchUserOption).pipe(
+      map(response => {
+
+        if (response.status) {
+          let external_users: Array<User> = [];
+          external_users = response.result;
+          return external_users;
+        } else {
+          return []
+        }
+      }, (error: any) => {
+        this.notificationService.error(error);
+        this.authService.raiseError();
+      }))
+
+  }
+
   ngOnInit(): void {
   }
 
-  print(){
-
+  print() {
   }
 
-  save(){
+  save() {
 
     this.invoice.id = this.invoice_id;
+    this.invoice.external_user_id = this.externalUser?.id;
 
-    // if(this.invoice.serie || this.invoice.client){
-    if(this.invoice.serie){
-      this.invoice.serie = this.invoice.serie.toUpperCase();
-      this.updateInvoice(this.invoice);
-    }else{
-      this.activeModal.close(true);
-    }
+    this.invoice.serie = this.invoice.serie?.toUpperCase();
+    this.updateInvoice(this.invoice);
+
   }
 
-  updateInvoice(invoice: SellInvoice){
+  updateInvoice(invoice: SellInvoice) {
     this.invoiceService.update(invoice).subscribe(response => {
 
       if (response.status) {
