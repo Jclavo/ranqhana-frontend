47f5ea6ce0c8fd43d7e753be3fb79c261b138993
Jclavo <jclavotafur@gmail.com>
2020-08-16 18:03:27 -0300

    add static method to get Role Client ID

--- src/modules/roles/models/role.model.ts
+++ src/modules/roles/models/role.model.ts
@@ -3,5 +3,13 @@ export class Role {
     id: number = 0;
     name: string = '';
     project_id: number = 0;
+
+    /**
+     * static setters
+     */
+
+    static getClientID(){
+        return 4;
+    }
     
 }
