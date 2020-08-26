261e24aac10716d22fc7f5d2af4231869980af90
Jclavo <jclavotafur@gmail.com>
2020-08-26 18:01:41 -0300

    add hardcode if field icon is empty in ModuleModel

--- src/modules/modules/models/module.model.ts
+++ src/modules/modules/models/module.model.ts
@@ -2,7 +2,7 @@ export class Module{
     id: number = 0;
     name: string = '';
     url: string = '';
-    icon: string = '';
+    icon: string = 'table';
     project_id: number = 0;
     parent_id: number = 0;
     labeled: boolean = false;
