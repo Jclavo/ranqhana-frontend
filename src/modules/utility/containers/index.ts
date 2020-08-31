aa412ba5888cb2373dc9c3aabd944bdd0785b3f5
Jclavo <jclavotafur@gmail.com>
2020-08-31 18:34:14 -0300

    create html in Utility to store i18n tag according

--- src/modules/utility/containers/index.ts
+++ src/modules/utility/containers/index.ts
@@ -1,10 +1,13 @@
 import { VersionComponent } from './version/version.component';
-import { FormValidationsComponent } from './form-validations/form-validations.component';
+import { FormValidationsComponent } from './i18n/form-validations/form-validations.component';
+import { ButtonsComponent } from './i18n/buttons/buttons.component';
 
 export const containers = [
     VersionComponent,
     FormValidationsComponent,
+    ButtonsComponent
 ];
 
 export * from './version/version.component';
-export * from './form-validations/form-validations.component';
+export * from './i18n/form-validations/form-validations.component';
+export * from './i18n/buttons/buttons.component';
