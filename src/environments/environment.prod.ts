cd310ce28c6ef58eede4cd865f5e68ba96fd86b9
Jclavo <jclavotafur@gmail.com>
2020-09-10 22:08:03 -0300

    update env file for PRD

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
     production: true,
+    apiURLTaapaq: 'http://127.0.0.1:8080/api/',
+    //apiURL: 'https://blooming-hamlet-62206.herokuapp.com/api/',
+    apiURL: 'http://127.0.0.1:8000/api/',
 };
