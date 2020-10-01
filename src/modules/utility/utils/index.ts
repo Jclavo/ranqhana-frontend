7ff4f1e35ca87bcd7e334f362168d4f431509f3e
Jclavo <jclavotafur@gmail.com>
2020-10-01 20:32:06 -0300

    create function to encrypt with laravel mode

--- src/modules/utility/utils/index.ts
+++ src/modules/utility/utils/index.ts
@@ -1,7 +1,9 @@
 import { FormUtils } from './form-utils';
 import { CustomValidator } from './custom-validator';
+import { LaravelEncrypt } from './laravel-encrypt';
 
-export const utils = [FormUtils, CustomValidator]
+export const utils = [FormUtils, CustomValidator, LaravelEncrypt]
 
 export * from './form-utils';
 export * from './custom-validator';
+export * from './laravel-encrypt';
---
+++ src/modules/utility/utils/laravel-encrypt.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+
+//models
+// import { Mask } from '../models';
+import * as CryptoJS from 'crypto-js';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class LaravelEncrypt {
+
+    static encrypt(data: any) {
+
+        // let laravelKEY = 'I4Sbr8hMimSxpmRG7GtTbcnjUC/+d7Q4haqrTFgJNB8=';
+        // let iv : any;
+        // let encrypted : any;
+        // iv = CryptoJS.lib.WordArray.random(16);
+        // // key = CryptoJS.enc.Utf8.parse(laravelKEY);
+        // laravelKEY = CryptoJS.enc.Base64.parse(laravelKEY);
+        // let options = {
+        //     iv: iv,
+        //     mode: CryptoJS.mode.CBC,
+        //     padding: CryptoJS.pad.Pkcs7
+        // };
+        // encrypted = CryptoJS.AES.encrypt(JSON.stringify(data), laravelKEY, options);
+        // encrypted = encrypted.toString();
+        // iv = CryptoJS.enc.Base64.stringify(iv);
+
+        // let result = {
+        //     iv: iv,
+        //     value: encrypted,
+        //     mac: CryptoJS.HmacSHA256(iv + encrypted, laravelKEY).toString()
+        // }
+
+        // let finalResult = JSON.stringify(result);
+        // finalResult = CryptoJS.enc.Utf8.parse(finalResult);
+        // return CryptoJS.enc.Base64.stringify(finalResult);
+
+
+
+        let laravelKEY = 'I4Sbr8hMimSxpmRG7GtTbcnjUC/+d7Q4haqrTFgJNB8=';
+        let iv: any;
+        let encrypted: any;
+
+        iv = CryptoJS.lib.WordArray.random(16),
+        // laravelKEY = CryptoJS.enc.Utf8.parse(laravelKEY);
+        laravelKEY = CryptoJS.enc.Base64.parse(laravelKEY);
+        let options = {
+            iv: iv,
+            mode: CryptoJS.mode.CBC,
+            padding: CryptoJS.pad.Pkcs7
+        };
+        encrypted = CryptoJS.AES.encrypt(JSON.stringify(data), laravelKEY, options);
+        encrypted = encrypted.toString();
+        iv = CryptoJS.enc.Base64.stringify(iv);
+        let result = {
+            iv: iv,
+            value: encrypted,
+            mac: CryptoJS.HmacSHA256(iv + encrypted, laravelKEY).toString()
+        }
+        let finalResult = JSON.stringify(result);
+        finalResult = CryptoJS.enc.Utf8.parse(finalResult);
+        return CryptoJS.enc.Base64.stringify(finalResult);
+    };
+
+
+}
