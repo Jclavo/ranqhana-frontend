b452f07d0b83eeff4025278770487833e54029c5
Jclavo <jclavotafur@gmail.com>
2020-10-06 20:14:57 -0300

    add ft to create and delete image in its service

--- src/modules/utility/models/image.model.ts
+++ src/modules/utility/models/image.model.ts
@@ -1,16 +1,20 @@
 export class Image {
 
-    public name: string;
-    public image: File;
-    public path: string;
-    public fullPath: string;
+    id: number = 0;
+    name: string = "";
 
-    constructor(name: string, image: File, path: string, fullPath: string )
-    {
-        this.name = name;
-        this.image = image;
-        this.path = path;
-        this.fullPath = fullPath;
-    }
+    model_id : number = 0;
+    model: string = "";
+    // public image: File;
+    // public path: string;
+    // public fullPath: string;
+
+    // constructor(name: string, image: File, path: string, fullPath: string )
+    // {
+    //     this.name = name;
+    //     this.image = image;
+    //     this.path = path;
+    //     this.fullPath = fullPath;
+    // }
 
 }
--- src/modules/utility/services/image.service.ts
+++ src/modules/utility/services/image.service.ts
@@ -7,7 +7,7 @@ import { map, catchError, switchMap } from "rxjs/operators";
 import { environment } from "../../../environments/environment";
 
 //Models
-import { Response } from '@modules/utility/models';
+import { Response, Image } from '@modules/utility/models';
 
 //SERVICES
 import { AuthService } from '@modules/auth/services';
@@ -19,7 +19,8 @@ export class ImageService {
 
   static service: string = 'images/';
 
-  private apiRoot: string = environment.apiURLTaapaq + ImageService.service;
+  private apiRootExternal: string = environment.apiURLTaapaq + ImageService.service;
+  private apiRoot: string = environment.apiURL + ImageService.service;
 
   constructor(
     private http: HttpClient,
@@ -27,11 +28,9 @@ export class ImageService {
   ) {
   }
 
-  save(image: any): Observable<Response> {
+  create(image: any): Observable<Response> {
 
-    let apiRoot = this.apiRoot;
-
-    return this.http.post(apiRoot, image, this.authService.getHeaders()).pipe(map(res => {
+    return this.http.post(this.apiRootExternal, image, this.authService.getHeaders()).pipe(map(res => {
 
       let response = new Response();
       let resultRAW: any = res;
@@ -44,6 +43,48 @@ export class ImageService {
         response.result = resultRAW.result?.name;
       }
 
+      return response;
+
+    }),
+      catchError(error => {
+        return throwError(error.message);
+      }));
+  }
+
+  save(image: Image): Observable<Response> {
+
+    let apiRoot = this.apiRoot;
+
+    return this.http.post(apiRoot, image, this.authService.getHeaders()).pipe(map(res => {
+
+      let response = new Response();
+      let resultRAW: any = res;
+
+      //Set response
+      response.status = resultRAW.status;
+      response.message = resultRAW.message;
+      //response.result = resultRAW.result;
+
+      return response;
+
+    }),
+      catchError(error => {
+        return throwError(error.message);
+      }));
+  }
+
+  delete(id: string): Observable<Response> {
+
+    let apiRoot = this.apiRoot + id;
+
+    return this.http.delete(apiRoot, this.authService.getHeaders()).pipe(map(res => {
+
+      let response = new Response();
+      let resultRAW: any = res;
+
+      //Set response
+      response.status = resultRAW.status;
+      response.message = resultRAW.message;
       return response;
 
     }),
