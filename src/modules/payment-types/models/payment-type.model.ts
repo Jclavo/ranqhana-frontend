8f632be4f67a0c607f84a74968e9d49cb1ada6db
Jclavo <jclavotafur@gmail.com>
2020-10-09 20:44:40 -0300

    add getter to Payment type model

--- src/modules/payment-types/models/payment-type.model.ts
+++ src/modules/payment-types/models/payment-type.model.ts
@@ -1,4 +1,13 @@
 export class PaymentType {
     id: string = '';
     name: string = '';
+
+
+    static getTypeDebit(){
+        return 1;
+    }
+
+    static getTypeCredit(){
+        return 2;
+    }
 }
