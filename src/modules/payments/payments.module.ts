a6b83f2f2f91b42255c70547abb7a4a7cfe6b38c
Jclavo <jclavotafur@gmail.com>
2020-10-13 10:58:52 -0300

    import confirm modal in Payment module

--- src/modules/payments/payments.module.ts
+++ src/modules/payments/payments.module.ts
@@ -7,9 +7,11 @@ import { HttpClientModule } from '@angular/common/http';
 /* Modules */
 import { AppCommonModule } from '@common/app-common.module';
 import { NavigationModule } from '@modules/navigation/navigation.module';
+import { UtilityModule } from '@modules/utility/utility.module';
 
 /* Components */
 import * as paymentsComponents from './components';
+import * as utilityComponents from '@modules/utility/components';
 
 /* Containers */
 import * as paymentsContainers from './containers';
@@ -29,6 +31,7 @@ import * as paymentsServices from './services';
     HttpClientModule,
     AppCommonModule,
     NavigationModule,
+    UtilityModule
   ],
   providers: [
     DecimalPipe,
@@ -43,6 +46,7 @@ import * as paymentsServices from './services';
   exports: [...paymentsContainers.containers, ...paymentsComponents.components],
   entryComponents: [
     paymentsComponents.MadePaymentModalComponent,
+    utilityComponents.ConfirmModalComponent,
   ]
 })
 export class PaymentsModule { }
