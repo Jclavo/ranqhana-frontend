35a9e0298ce36bb83cbea26eef9d0c4ab828510f
Jclavo <jclavotafur@gmail.com>
2020-10-21 15:27:45 -0300

    add feature to get all order stages

--- src/modules/order-stages/services/order-stage.service.ts
+++ src/modules/order-stages/services/order-stage.service.ts
@@ -1,9 +1,60 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from "rxjs/operators";
+
+//ENVIRONMENT
+import { environment } from "../../../environments/environment";
+
+//Models
+import { OrderStage } from '../models';
+import { Response } from '../../utility/models';
+import { AuthService } from '@modules/auth/services';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderStageService {
 
-  constructor() { }
+  static service: string = 'order-stages/'
+  private apiRoot: string = environment.apiURL + OrderStageService.service;
+
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService,  
+  ) { }
+
+  getAll() : Observable<Response> { 
+
+    let apiRoot = this.apiRoot;
+
+    return this.http.get(apiRoot, this.authService.getHeaders()).pipe(map(res => {
+
+      let response = new Response();
+      let resultRAW: any = res;
+
+      //Set response
+      response.status = resultRAW.status;
+      response.message = resultRAW.message;
+
+      response.result = resultRAW.result.map((item: any) => {
+
+        let orderStage = new OrderStage();
+        orderStage.id = item.id;
+        orderStage.name = item.name;
+
+        return orderStage;
+      });
+
+      response.records = resultRAW.result.length;
+
+      return response;
+
+    }),
+      catchError(error => {
+        return throwError(error.message);
+      }));
+  }
+
+
 }
