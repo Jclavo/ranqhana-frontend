87c846ef776032b3989c1e2a285856e255f5dcb5
Jclavo <jclavotafur@gmail.com>
2020-10-26 17:37:17 -0300

    add InvoiceType for Invoices Models

--- src/modules/invoices/models/PurchaseInvoice.model.ts
+++ src/modules/invoices/models/PurchaseInvoice.model.ts
@@ -1,9 +1,11 @@
 import { Invoice } from "./Invoice.model";
+import { InvoiceType } from "@modules/invoice-types/models";
 
 export class PurchaseInvoice extends Invoice {
 
     constructor(){
         super();
+        this.type_id = InvoiceType.getForPurchase();
     }
      
 }
--- src/modules/invoices/models/SellInvoice.model.ts
+++ src/modules/invoices/models/SellInvoice.model.ts
@@ -1,10 +1,11 @@
 import { Invoice } from "./Invoice.model";
-// import { Invoice } from "./Invoice.model";
+import { InvoiceType } from "@modules/invoice-types/models";
+
 export class SellInvoice extends Invoice {
 
     constructor(){
         super();
-        this.type_id = 1;
+        this.type_id = InvoiceType.getForSell();
     }
      
 }
