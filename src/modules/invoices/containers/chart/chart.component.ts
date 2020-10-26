0afbd0867ca32f78f9e9ce032a0f11a772673af8
Jclavo <jclavotafur@gmail.com>
2020-10-26 18:03:34 -0300

    get InvoiceTypes from db in Chart Invoice

--- src/modules/invoices/containers/chart/chart.component.ts
+++ src/modules/invoices/containers/chart/chart.component.ts
@@ -5,10 +5,12 @@ import { InvoiceService } from "../../services/invoice.service";
 import { ReportService } from "@modules/reports/services";
 import { NotificationService, CustomDateService } from '@modules/utility/services';
 import { AuthService } from "@modules/auth/services";
+import { InvoiceTypeService } from "@modules/invoice-types/services";
 
 //MODEL
 import { SearchInvoice } from "../../models";
 import { Graphic } from '@modules/utility/models';
+import { InvoiceType } from '@modules/invoice-types/models';
 
 @Component({
   selector: 'sb-chart',
@@ -19,19 +21,39 @@ export class ChartComponent implements OnInit {
 
   public searchInvoiceOption = new SearchInvoice();
   public graphicData: Array<Graphic> = [];
+  public invoiceTypes: Array<InvoiceType> = [];
 
   constructor(
     private invoiceService: InvoiceService,
     private notificationService: NotificationService,
     private authService: AuthService,
     private customDateService: CustomDateService,
-    private reportService: ReportService
+    private reportService: ReportService,
+    private invoiceTypeService: InvoiceTypeService,
     ) { 
       this.searchInvoiceOption.fromDate = this.searchInvoiceOption.toDate = this.customDateService.getToday();
     }
 
   ngOnInit(): void {
     this.search();
+    this.getInvoiceTypes();
+  }
+
+  getInvoiceTypes() {
+
+    this.invoiceTypeService.getAll().subscribe(response => {
+
+      if (response.status) {
+        this.invoiceTypes = response.result;
+      } else {
+        this.notificationService.error(response.message);
+      }
+
+    }, error => {
+      this.notificationService.error(error);
+      this.authService.raiseError();
+    });
+
   }
 
 
