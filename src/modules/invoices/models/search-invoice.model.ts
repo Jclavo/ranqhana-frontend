fd95d79bea581f65796972dacb26c92d9d134a20
Jclavo <jclavotafur@gmail.com>
2020-11-13 13:06:11 -0300

    set type_id to 0 in search invoice model

--- src/modules/invoices/models/search-invoice.model.ts
+++ src/modules/invoices/models/search-invoice.model.ts
@@ -4,7 +4,7 @@ export class SearchInvoice extends SearchOptions {
 
     fromDate: string = '';
     toDate: string = '';
-    type_id: number = 1;
+    type_id: number = 0;
     searchBy: string = 'H';
 
     constructor() {
