dc646d4fe499b385ee2b33aef868e03862068071
Jclavo <jclavotafur@gmail.com>
2020-11-16 16:39:46 -0300

    copy url image to env for prd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,5 @@ export const environment = {
     apiURLTaapaq: 'http://127.0.0.1:8080/api/',
     //apiURL: 'https://blooming-hamlet-62206.herokuapp.com/api/',
     apiURL: 'http://127.0.0.1:8000/api/',
+    apiURLImage: 'http://127.0.0.1:8080/images/',
 };
